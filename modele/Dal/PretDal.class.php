6f55763443e06f9a5c85d99196fd4604d5641296
Vin57 <vincphil54800@gmail.com>
2016-10-03 18:06:25 +0200

    Ajout de la fonction loadGenericPret
    loadGenericPret est une fonction générique de charger de prêts

--- modele/Dal/PretDal.class.php
+++ modele/Dal/PretDal.class.php
@@ -31,6 +31,58 @@ class PretDal {
         }        
         return $res;
     }
+    
+    /**
+     * loadGenericPret est une fonction générique de charger de prêts
+     * @param int $mode 0 => charge tous les prêts de tous les clients avec etat optionel, 1 => charge un 
+     * prêts par id, 2 => charge tous les prêts d'un clients par etat
+     * @param array $arrayParams si mode 0 donné un état de prêts (voir config.inc.php), si mode 1 mettre
+     * un id de pret dans le tableau, si mode 2 mettre un numéro de client et un état de prêts (voir config.inc.php)
+     * @return mixed 
+     */
+    public static function loadGenericPret($mode,$arrayParams) {
+        
+        $cnx = new PdoDao();
+        
+        $id = null;
+        $etat = null;
+        
+        switch($mode)
+        {
+            case 0:{
+                if(count($arrayParams)==1)
+                {
+                    $etat = $arrayParams[0];
+                }
+            }break;
+            case 1:{
+                if(count($arrayParams)==1)
+                {
+                    $id = $arrayParams[0];
+                }
+                else{
+                    return false;
+                }
+            }break;
+            case 2:{
+                if(count($arrayParams)==2)
+                {
+                    $id = $arrayParams[0];
+                    $etat = $arrayParams[1];
+                }
+                else{
+                    return false;
+                }
+            }break;
+        }
+        $qry = 'CALL sp_pret(?,?)';
+        $res = $cnx->getRows($qry, array($mode,$id,$etat), 1);
+        if (is_a($res, 'PDOException')) {
+            return PDO_EXCEPTION_VALUE;
+        }
+        return $res;
+    }
+    
     /**
      * charge un objet de la classe Pret à partir de son id
      * @param  int	$id : l'identifiant du pret
